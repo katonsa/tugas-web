3971df0bd28577b5dc29baba010f8c8d57daed17
Katon Surya Atmaja <katonatmaja@gmail.com>
2021-06-17 07:54:27 +0700

    Menambahkan library WS
    Untuk membuat fungsi realtime antar tiap client.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express =  require('express');
 const cors = require('cors');
+const WebSocket = require('ws');
+const http = require('http');
 
 const app = express();
 app.use(cors());
@@ -25,6 +27,26 @@ app.get('/', (req,res) => {
   )
 });
 
-app.listen(3000, function() {
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ noServer: true });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
+server.on('upgrade', function (request, socket, head) {
+  wss.handleUpgrade(request, socket, head, function (ws) {
+    wss.emit('connection', ws, request);
+  });
+});
+
+server.listen(3000, function() {
   console.log('> Server listening on 3000...');
 });
